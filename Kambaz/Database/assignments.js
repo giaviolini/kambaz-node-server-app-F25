71ea3d47dabb8517c903f8dd933d3171b68e3bff
Gia Violini <g1aviolini123@gmail.com>
2025-11-29 21:04:19 -0500

    first commit

---
+++ Kambaz/Database/assignments.js
@@ -0,0 +1,10 @@
+export default [  { "_id": "A101", "title": "Propulsion Assignment", "course": "RS101", "points": 100, "startDate": "2025-05-06T00:00:00-04:00", "endDate": "2025-05-13T11:59:00-04:00"},
+    { "_id": "A102", "title": "Combustion Analysis", "course": "RS101", "points": 100, "startDate": "2025-05-14T00:00:00-04:00", "endDate": "2025-05-21T11:59:00-04:00"},
+    { "_id": "A103", "title": "Nozzle Design Project", "course": "RS101", "points": 100, "startDate": "2025-05-21T00:00:00-04:00", "endDate": "2025-05-27T11:59:00-04:00" },
+    { "_id": "A201", "title": "Aerodynamics Quiz", "course": "RS102", "points": 100, "startDate": "2025-05-06T00:00:00-04:00", "endDate": "2025-05-13T11:59:00-04:00"},
+    { "_id": "A202", "title": "Flow Analysis", "course": "RS102", "points": 100, "startDate": "2025-05-14T00:00:00-04:00", "endDate": "2025-05-21T11:59:00-04:00"},
+    { "_id": "A203", "title": "Heating Analysis", "course": "RS102", "points": 100, "startDate": "2025-05-21T00:00:00-04:00", "endDate": "2025-05-27T11:59:00-04:00" },
+    { "_id": "A301", "title": "Structural Design Task", "course": "RS103", "points": 100, "startDate": "2025-05-06T00:00:00-04:00", "endDate": "2025-05-13T11:59:00-04:00"},
+    { "_id": "A302", "title": "Orbital Calculations", "course": "RS103", "points": 100, "startDate": "2025-05-14T00:00:00-04:00", "endDate": "2025-05-21T11:59:00-04:00"},
+    { "_id": "A303", "title": "Systems Engineering Exam", "course": "RS103", "points": 100, "startDate": "2025-05-21T00:00:00-04:00", "endDate": "2025-05-27T11:59:00-04:00"}
+  ];
